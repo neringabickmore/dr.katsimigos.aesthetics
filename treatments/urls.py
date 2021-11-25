36b6ab8623c22bea7c1275161da5176fbc4ddf7d
neringa <neringa.bickmore@gmail.com>
2021-11-25 17:07:04 +0000

    treatment-details.html/added basic content and url

--- treatments/urls.py
+++ treatments/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = [
     path('treatments/', views.view_treatments, name= 'view_treatments'),
+    path('treatment-details/', views.treatment_details, name= 'treatment_details'),
 ]
--- treatments/views.py
+++ treatments/views.py
@@ -6,3 +6,10 @@ def view_treatments(request):
     template = 'treatments/treatments.html'
     
     return render (request, template)
+
+
+def treatment_details(request):
+    
+    template = 'treatments/treatment-details.html'
+    
+    return render (request, template)
