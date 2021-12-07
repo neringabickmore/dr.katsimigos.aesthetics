550f168a6abcd9558536f3f6c98b03b72dd82b9b
neringa <neringa.bickmore@gmail.com>
2021-12-07 23:54:37 +0000

    edit-contacts/added edit functionality to contact details

--- about/models.py
+++ about/models.py
@@ -16,3 +16,24 @@ class About(models.Model):
     def __str__(self):
         return self.name
 
+
+class Contact(models.Model):
+    """
+    Contact Section Model in About page.
+    """
+
+    class Meta:
+        verbose_name_plural = "Contact Section"
+
+    header = models.CharField(max_length=254)
+    subheader = models.TextField(max_length=254)
+    telephone_number = models.CharField(
+        max_length=20, null=False, blank=False)
+    email_address = models.EmailField(
+        max_length=254, null=False, blank=False)
+    instagram_handle = models.URLField(
+        max_length=1024, default='', null=True, blank=True)
+    
+    def __str__(self):
+        return self.header
+
--- about/urls.py
+++ about/urls.py
@@ -5,4 +5,5 @@ from . import views
 urlpatterns = [
     path('', views.view_about, name= 'view_about'),
     path('edit/about/<int:about_id>/', views.edit_about, name='edit_about'),
+    path('edit/contact/<int:contact_id>/', views.edit_contact, name='edit_contact'),
 ]
