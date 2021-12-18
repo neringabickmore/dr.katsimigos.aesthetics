aedf05ac3aa5c260287aebfcb11884dd8492e4a7
neringa <neringa.bickmore@gmail.com>
2021-12-18 18:28:08 +0000

    CNG/add-new-treatment URL

--- treatments/urls.py
+++ treatments/urls.py
@@ -5,5 +5,5 @@ from . import views
 urlpatterns = [
     path('treatments/', views.view_treatments, name= 'view_treatments'),
     path('<name>/', views.treatment_details, name= 'treatment_details'),
-    path('add-new-treatment/', views.add_new_treatment, name= 'add_new_treatment'),
+    path('add/new/treatment/', views.add_new_treatment, name= 'add_new_treatment'),
 ]
