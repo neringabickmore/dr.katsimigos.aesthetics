d414fc99bebdba3f814ecc098a305c6baac70d73
neringa <neringa.bickmore@gmail.com>
2021-12-19 15:57:07 +0000

    CNG/treatment model and dependencies

--- treatments/admin.py
+++ treatments/admin.py
@@ -3,15 +3,25 @@ from .models import TreatmentDetails
 
 class TreatmentsAdmin(admin.ModelAdmin):
     """
-    ATreatments section admin
+    Treatments admin
     """
 
     list_display = (
         'treatment_name',
-        'description',
+        'treatment_description',
+        'main_picture',
         'before_picture',
         'after_picture',
-        'display',
+        'display_treatment',
+        'treatment_price_description',
+        'price_name_a',
+        'price_a',
+        'price_name_b',
+        'price_b',
+        'price_name_c',
+        'price_c',
+        'price_name_d',
+        'price_d',
     )
 
 
--- treatments/models.py
+++ treatments/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.core.validators import MinValueValidator
 import cloudinary
 import cloudinary.uploader
 
@@ -6,11 +7,38 @@ class TreatmentDetails(models.Model):
     """
     A model to specify treatment details
     """
-    treatment_name = models.CharField(max_length=150, null=False, blank=False, unique=True)
-    description = models.CharField(max_length=1550, null=False, blank=False)
-    before_picture = models.ImageField(upload_to='before_picture/', blank=True)
-    after_picture = models.ImageField(upload_to='after_picture/', blank=True)
-    display = models.BooleanField(default=True)
+    class Meta:
+        verbose_name_plural = "Treatment Details"
+
+    treatment_name = models.CharField(
+        max_length=150, null=False, blank=False, unique=True)
+    treatment_description = models.CharField(
+        max_length=1550, null=False, blank=False)
+    main_picture = models.ImageField(
+        upload_to='main_picture/', blank=False)
+    before_picture = models.ImageField(
+        upload_to='before_picture/', blank=True)
+    after_picture = models.ImageField(
+        upload_to='after_picture/', blank=True)
+    display_treatment = models.BooleanField(default=True, blank=False)
+    treatment_price_description = models.CharField(
+        max_length=1550, null=True, blank=True)
+    price_name_a = models.CharField(
+        max_length=150, null=True, blank=False)
+    price_a = models.DecimalField(
+        max_digits=6, decimal_places=2, validators=[MinValueValidator(0.01)], blank=False, null=True)
+    price_name_b = models.CharField(
+        max_length=150, null=False, blank=True)
+    price_b = models.DecimalField(
+        max_digits=6, decimal_places=2, validators=[MinValueValidator(0.01)], blank=True, null=True)
+    price_name_c = models.CharField(
+        max_length=150, null=False, blank=True)
+    price_c = models.DecimalField(
+        max_digits=6, decimal_places=2, validators=[MinValueValidator(0.01)], blank=True, null=True)
+    price_name_d = models.CharField(
+        max_length=150, null=False, blank=True)
+    price_d = models.DecimalField(
+        max_digits=6, decimal_places=2, validators=[MinValueValidator(0.01)], blank=True, null=True)
 
     def __str__(self):
-        return self.description
+        return self.treatment_description
